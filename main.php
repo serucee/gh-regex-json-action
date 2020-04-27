fb7d9d049a7b8c163c27dc43ab1c2cc797e37cc7
Sebastian Russmann <43376129+serucee@users.noreply.github.com>
2020-04-27 10:46:46 +0200

    Fix json

--- main.php
+++ main.php
@@ -2,8 +2,8 @@
 $input = file_get_contents($_SERVER["GITHUB_WORKSPACE"] . "/files.json");
 $json = json_decode($input, true);
 $pattern = '/^[0-9]{3}-.*/u';
-
-foreach ($json['all'] as $fileName) {
+print_r($json);
+foreach ($json as $fileName) {
 	if(preg_match($pattern, $fileName) !== 1) {
 		exit(1);
 	}
