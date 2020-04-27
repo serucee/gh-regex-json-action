797822169e2bc6be8753e912b2772395886aef84
Sebastian Russmann <43376129+serucee@users.noreply.github.com>
2020-04-27 11:12:44 +0200

    Fix regex

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
 $input = file_get_contents($_SERVER["GITHUB_WORKSPACE"] . "/files.json");
 $json = json_decode($input, true);
-$pattern = '/^.*\/[0-9]{3}-.*/u';
+$pattern = '/^.*\/*[0-9]{3}-.*/u';
 foreach ($json as $fileName) {
 	if(preg_match($pattern, $fileName) !== 1) {
 		echo "Filename $fileName invalid!";
