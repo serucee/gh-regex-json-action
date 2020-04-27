65e8383b718bccd9869da8d26e52355eabf01070
Sebastian Russmann <43376129+serucee@users.noreply.github.com>
2020-04-27 10:48:33 +0200

    Add error msg

--- main.php
+++ main.php
@@ -2,9 +2,9 @@
 $input = file_get_contents($_SERVER["GITHUB_WORKSPACE"] . "/files.json");
 $json = json_decode($input, true);
 $pattern = '/^[0-9]{3}-.*/u';
-print_r($json);
 foreach ($json as $fileName) {
 	if(preg_match($pattern, $fileName) !== 1) {
+		echo "$fileName invalid!";
 		exit(1);
 	}
 }
