7f016afe0a5da3359c3cf2cb96ba22feba642a6a
Sebastian Russmann <43376129+serucee@users.noreply.github.com>
2020-04-27 10:27:58 +0200

    Debug

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM php:7.4-cli
 
-COPY /github/home/ /github/home/
 COPY . /usr/local/bin
 
 CMD [ "php", "/usr/local/bin/main.php" ]
--- main.php
+++ main.php
@@ -1,5 +1,4 @@
 <?php
-shell_exec("cat /github/home/files.json");
 $input = file_get_contents("/github/home/files.json");
 $json = json_decode($input, true);
 $pattern = '/^[0-9]{3}-.*/u';
